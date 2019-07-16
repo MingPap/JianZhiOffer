85ba174ff795ae1742972bde56e850cb0670f41f
Ming_papa <936330158@qq.com>
2019-07-16 10:34:26 +0800

    判断边界条件

--- 27_二叉树的镜像.cpp
+++ 27_二叉树的镜像.cpp
@@ -1,29 +1,32 @@
-﻿#include<iostream>
-struct TreeNode {
-	int val;
-	struct TreeNode *left;
-	struct TreeNode *right;
-	TreeNode(int x) :
-	val(x), left(NULL), right(NULL) {
-	}
-};
+﻿
+
 class Solution {
 public:
-	void MirrorCore(TreeNode* pnode)
-	{
-		if (pnode)
+	vector<int> printMatrix(vector<vector<int> > matrix) {
+		int row = matrix.size();
+		int col = matrix[0].size();
+		vector<int> result;
+		if (row == 0 || col == 0)
+			return result;
+		int left = 0, right = col - 1, up = 0, down = row - 1;
+		while (left <= right&&up <= down)
 		{
-			TreeNode* t = pnode->left;
-			pnode->left = pnode->right;
-			pnode->right = t;
-			MirrorCore(pnode->left);
-			MirrorCore(pnode->right);
+			for (int i = left; i <= right; ++i)
+				result.push_back(matrix[up][i]);
+			if (up<down)
+				for (int i = up + 1; i <= down; ++i)
+					result.push_back(matrix[i][right]);
+			if (right>left&&up<down)
+				for (int i = right - 1; i >= left; --i)
+					result.push_back(matrix[down][i]);
+			if (down>up + 1 && right>left)
+				for (int i = down - 1; i >= up + 1; --i)
+					result.push_back(matrix[i][left]);
+			++left;
+			--right;
+			++up;
+			--down;
 		}
-	}
-	void Mirror(TreeNode *pRoot) {
-		if ((!pRoot) || (!pRoot->left && !pRoot->right))
-			return;
-		MirrorCore(pRoot);
-
+		return result;
 	}
 };
