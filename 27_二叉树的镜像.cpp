a75a62ef6a6b83933a263add54c0af60e1bffee5
Ming_papa <936330158@qq.com>
2019-07-16 10:00:46 +0800

    前序遍历&递归

---
+++ 27_二叉树的镜像.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+	val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+	void MirrorCore(TreeNode* pnode)
+	{
+		if (pnode)
+		{
+			TreeNode* t = pnode->left;
+			pnode->left = pnode->right;
+			pnode->right = t;
+			MirrorCore(pnode->left);
+			MirrorCore(pnode->right);
+		}
+	}
+	void Mirror(TreeNode *pRoot) {
+		if ((!pRoot) || (!pRoot->left && !pRoot->right))
+			return;
+		MirrorCore(pRoot);
+
+	}
+};
